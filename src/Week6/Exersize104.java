ab27fb7e46705d96ec4f5bdecbad87a126c943b8
oliver <trojanowskioliver1@gmail.com>
2022-10-08 12:01:47 +0200

    1

---
+++ src/Week6/Exersize104.java
@@ -0,0 +1,68 @@
+package Week6;
+
+import java.util.Arrays;
+
+public class Exersize104 {
+    public static int smallest(int[] array) {
+        int smallest = Integer.MAX_VALUE;
+        for (int i: array) {
+            if (i < smallest) {
+                smallest = i;
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfTheSmallest(int[] array) {
+        int smallest = smallest(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == smallest) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int indexOfTheSmallestStartingFrom(int[] array, int value) {
+        int[] newArray = new int[array.length - value];
+        for (int i = 0; i < array.length; i++) {
+            if (i >= value) {
+                newArray[i - value] = array[i];
+            }
+        }
+
+        return indexOfTheSmallest(newArray) + value;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int num1 = array[index1];
+        int num2 = array[index2];
+
+        if (num1 != num2) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == num1) {
+                    array[i] = num2;
+                } else if (array[i] == num2) {
+                    array[i] = num1;
+                }
+            }
+        }
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int smallest = indexOfTheSmallestStartingFrom(array, i);
+            swap(array, i, smallest);
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] values = {8, 34, 8, 5, 1, 3, 4, 15423, 42};
+
+        System.out.println(Arrays.toString(values));
+        sort(values);
+
+    }
+}
